6f1f25c467233f15f88d4ab58f0326b53a3e1117
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-27 14:55:30 +0530

    Signature View

--- SignatureView/ViewController.swift
+++ SignatureView/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var signatureView: YPDrawSignatureView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setting this view controller as the signature view delegate, so the didStart() and
+        // didFinish() methods below in the delegate section are called.
+        signatureView.delegate = self as? YPSignatureDelegate
+        
     }
+    
+    // Function for clearing the content of signature view
+    @IBAction func clearSignature(_ sender: UIButton) {
+        // This is how the signature gets cleared
+        self.signatureView.clear()
+    }
+    
+    // Function for saving signature
+    @IBAction func saveSignature(_ sender: UIButton) {
+        // Getting the Signature Image from self.drawSignatureView using the method getSignature().
+        if let signatureImage = self.signatureView.getSignature(scale: 10) {
+            
+            // Saving signatureImage from the line above to the Photo Roll.
+            // The first time you do this, the app asks for access to your pictures.
+            UIImageWriteToSavedPhotosAlbum(signatureImage, nil, nil, nil)
+            
+            // Since the Signature is now saved to the Photo Roll, the View can be cleared anyway.
+            self.signatureView.clear()
+        }
+    }
+    
+    // MARK: - Delegate Methods
+    
+    // The delegate functions gives feedback to the instanciating class. All functions are optional,
+    // meaning you just implement the one you need.
+    
+    // didStart(_ view: YPDrawSignatureView) is called right after the first touch is registered in the view.
+    // For example, this can be used if the view is embedded in a scroll view, temporary
+    // stopping it from scrolling while signing.
+    func didStart(_ view: YPDrawSignatureView) {
+        print("Started Drawing")
+    }
+    
+    // didFinish(_ view: YPDrawSignatureView) is called rigth after the last touch of a gesture is registered in the view.
+    // Can be used to enabe scrolling in a scroll view if it has previous been disabled.
+    func didFinish(_ view: YPDrawSignatureView) {
+        print("Finished Drawing")
+    }
+    
 
+    
 
 }
 
